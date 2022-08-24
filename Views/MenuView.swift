88f4976bf21e1cfc6ab5327cad915273a8c29fb9
Tadaki <tadaki741@gmail.com>
2022-08-24 18:04:30 +0700

    Add background image to the app

--- Views/MenuView.swift
+++ Views/MenuView.swift
@@ -18,69 +18,69 @@ struct MenuView: View {
     
     var body: some View {
         
-        //background image
-        Spacer();
-        //Display the welcome text
-        Text("Vietnamese Black Jack")
-                            .font(Font.system(size: 46, weight: .bold))
-                            .multilineTextAlignment(.center)
-                            .overlay {
-                                LinearGradient(
-                                    colors: [.red, .blue, .green, .yellow],
-                                    startPoint: .leading,
-                                    endPoint: .trailing
-                                )
-                                .mask(
-                                    Text("Vietnamese Black Jack")
-                                        .font(Font.system(size: 46, weight: .bold))
-                                        .multilineTextAlignment(.center)
-                                )
-                            }
-        NavigationView{
-            
-            
-            VStack{
+        VStack{
+            Spacer();
+            //Display the welcome text
+            Text("Vietnamese Black Jack")
+                                .font(Font.system(size: 46, weight: .bold))
+                                .multilineTextAlignment(.center)
+                                .overlay {
+                                    LinearGradient(
+                                        colors: [.red, .blue, .green, .yellow],
+                                        startPoint: .leading,
+                                        endPoint: .trailing
+                                    )
+                                    .mask(
+                                        Text("Vietnamese Black Jack")
+                                            .font(Font.system(size: 46, weight: .bold))
+                                            .multilineTextAlignment(.center)
+                                    )
+                                }
+            NavigationView{
                 
-                //MARK: PLAY THE GAME
-                NavigationLink(destination: askPlayerNameView(coreDM: coreDM),label:{
-                    Text("Play Game").bold()
-                        .frame(width: 280, height: 50)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10);
-                })
                 
-                //MARK: LEADERBOARD
-                NavigationLink(destination: LeaderboardView(coreDM: coreDM),label:{
-                    Text("LEADERBOARD").bold()
-                        .frame(width: 280, height: 50)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10);
-                })
-                
-                //MARK: SETTINGS
-                NavigationLink(destination: SettingsView(),label:{
-                    Text("Settings").bold()
-                        .frame(width: 280, height: 50)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10);
-                })
-                
-                //MARK: HOW TO PLAY
-                NavigationLink(destination: HowToPlayView(),label:{
-                    Text("How to play ?").bold()
-                        .frame(width: 280, height: 50)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10);
-                })
-                
+                VStack{
+                    
+                    //MARK: PLAY THE GAME
+                    NavigationLink(destination: askPlayerNameView(coreDM: coreDM),label:{
+                        Text("Play Game").bold()
+                            .frame(width: 280, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10);
+                    })
+                    
+                    //MARK: LEADERBOARD
+                    NavigationLink(destination: LeaderboardView(coreDM: coreDM),label:{
+                        Text("LEADERBOARD").bold()
+                            .frame(width: 280, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10);
+                    })
+                    
+                    //MARK: SETTINGS
+                    NavigationLink(destination: SettingsView(),label:{
+                        Text("Settings").bold()
+                            .frame(width: 280, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10);
+                    })
+                    
+                    //MARK: HOW TO PLAY
+                    NavigationLink(destination: HowToPlayView(),label:{
+                        Text("How to play ?").bold()
+                            .frame(width: 280, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10);
+                    })
+                }
             }
-            
-            
         }
+        
+        
     }
 }
 
