e632d2bd88d21ac8c092c98057222d255e322649
Tadaki <tadaki741@gmail.com>
2022-08-25 18:04:37 +0700

    + Add button to turn on/off sound

--- Views/SettingsView.swift
+++ Views/SettingsView.swift
@@ -12,24 +12,45 @@ struct SettingsView: View {
     @AppStorage("isDarkMode") private var isDarkMode = false;
     @AppStorage("enableMusic") private var enableMusic = false;
     @ObservedObject public var soundManager: SoundManager;
+    @State var soundON: Bool = false;
     
     var body: some View {
         
         
         
         
+        
         VStack{
+            Spacer();
+            Text("Game settings").fontWeight(.bold);
+            Spacer();
             Picker("Mode",selection: $isDarkMode){
                             Text("Light mode ☀️").tag(false);
                              Text("Dark mode 🌙").tag(true)
             }.pickerStyle(SegmentedPickerStyle()).padding();
             
+            Spacer();
+            //Button to turn on/off music
+            Button(soundON ? "MUSIC ON" : "MUSIC OFF"){
+                //If tap on the sound ON
+                if(soundON){
+                    soundManager.playBackgroundMusic();
+                    soundON = false;
+                }
+                //If tap on the sound OFF
+                else if(!soundON){
+                    soundManager.stopBackgroundMusic();
+                    soundON = true;
+                }
+            }.padding()
+            .frame(width: 280, height: 50)
+                .background(soundON ? Color.green : Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(10);
+            Spacer();
             
-            Picker("Mode",selection: $enableMusic){
-                             Text("Music ON").tag(true);
-                             Text("Music OFF").tag(false)
-            }.pickerStyle(SegmentedPickerStyle()).padding();
         }
+        
         
         
     }
